fdf9b8047a91c5deede5500d7953bc6f1a03bec0
Sajjad Hosseinzadeh <sajjad.hosseinzadeh@gmail.com>
2023-11-11 15:49:09 +0330

    first commit

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from apps.todo.routers import router as todo_router
+from config import settings
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
+    app.mongodb = app.mongodb_client[settings.DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+app.include_router(todo_router, tags=["tasks"], prefix="/task")
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        reload=settings.DEBUG_MODE,
+        port=settings.PORT,
+    )
